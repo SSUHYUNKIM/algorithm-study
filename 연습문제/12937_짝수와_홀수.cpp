ce43403eab138119892698a2acecae1a26cace1a
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-19 00:34:29 +0900

    [연습문제] 12937 짝수와 홀수

---
+++ 연습문제/12937_짝수와_홀수.cpp
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "Odd";
+
+    if(num % 2 == 0) {
+        answer = "Even";
+    }
+
+    return answer;
+}
