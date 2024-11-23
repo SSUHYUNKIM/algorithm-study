f6b80d9889ad0a253212630ef52d0d83be6453ec
SSUHYUNKIM <yellosuhyun@naver.com>
2024-11-23 11:31:48 +0900

    [연습문제] 12925 문자열을 정수로 바꾸기

---
+++ 연습문제/12925_문자열을_정수로_바꾸기.cpp
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    return stoi(s);
+}
